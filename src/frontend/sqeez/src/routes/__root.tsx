c4c174af8c2436084136ba2d5b4c1cdbbfc453c5
Denny Valach <denny.valach@gmail.com>
2026-02-09 20:44:04 +0100

    feat: i18next setup

--- src/frontend/sqeez/src/routes/__root.tsx
+++ src/frontend/sqeez/src/routes/__root.tsx
@@ -1,13 +1,31 @@
 import { Navbar } from '@/components/layouting/Navbar/Navbar'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { useTranslation } from 'react-i18next'
 
-const RootLayout = () => (
-  <>
-    <Navbar />
-    <Outlet />
-    <TanStackRouterDevtools />
-  </>
-)
+const navLinks = [
+  { to: '/', label: 'Home' },
+  { to: '/features', label: 'Features' },
+  { to: '/pricing', label: 'Pricing' },
+  { to: '/about', label: 'About' },
+]
+
+const RootLayout = () => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <Navbar
+        navLinks={navLinks}
+        title="Sqeez"
+        loginButtonText={t('common.signIn')}
+        registerButtonText={t('common.register')}
+        navigationText={t('navbar.navigation')}
+      />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  )
+}
 
 export const Route = createRootRoute({ component: RootLayout })

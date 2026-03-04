180f38e1342d075cc5c5c3082edd8211a0631c82
Denny Valach <xvalach3@fi.muni.cz>
2026-03-04 09:26:02 +0100

    fix: auth cookie removal

--- src/backend/Sqeez.Api/Services/AuthService/AuthService.cs
+++ src/backend/Sqeez.Api/Services/AuthService/AuthService.cs
@@ -11,21 +11,24 @@ namespace Sqeez.Api.Services.AuthService
     {
         private readonly SqeezDbContext _context;
         private readonly ITokenService _tokenService;
+        private readonly ILogger<AuthService> _logger;
 
-        public AuthService(SqeezDbContext context, ITokenService tokenService)
+        public AuthService(SqeezDbContext context, ITokenService tokenService, ILogger<AuthService> logger)
         {
             _context = context;
             _tokenService = tokenService;
+            _logger = logger;
         }
 
-        public async Task<string?> LoginAsync(LoginDTO loginDto)
+        public async Task<string?> LoginAsync(LoginDTO dto)
         {
+            _logger.LogInformation("Attempting to login user: {Email}", dto.Email);
             var user = await _context.Students
-                .FirstOrDefaultAsync(u => u.Email.ToLower() == loginDto.Email.ToLower());
+                .FirstOrDefaultAsync(u => u.Email == dto.Email.Trim().ToLower());
 
             if (user == null) return null;
 
-            bool isValid = BC.Verify(loginDto.Password.Trim(), user.PasswordHash);
+            bool isValid = BC.Verify(dto.Password.Trim(), user.PasswordHash);
 
             if (!isValid) return null;
 
@@ -36,14 +39,16 @@ namespace Sqeez.Api.Services.AuthService
             return _tokenService.CreateToken(user);
         }
 
-        public async Task<bool> RegisterAsync(RegisterDTO dto)
+        public async Task<string?> RegisterAsync(RegisterDTO dto)
         {
-            if (await _context.Students.AnyAsync(x => x.Email == dto.Email.ToLower()))
-                return false;
+            _logger.LogInformation("Attempting to register user: {Email}", dto.Email);
+
+            string email = dto.Email.Trim().ToLower();
+            if (await _context.Students.AnyAsync(x => x.Email == email))
+                return null;
 
             string salt = BC.GenerateSalt(12);
             string hashedPassword = BC.HashPassword(dto.Password.Trim(), salt);
-            string email = dto.Email.ToLower();
             string username = string.IsNullOrWhiteSpace(dto.Username) ? email.Split('@')[0] : dto.Username;
 
             var user = new Student
@@ -55,11 +60,16 @@ namespace Sqeez.Api.Services.AuthService
             };
 
             _context.Students.Add(user);
-            return await _context.SaveChangesAsync() > 0;
+
+            await _context.SaveChangesAsync();
+
+            return _tokenService.CreateToken(user);
         }
 
         public async Task<bool> LogoutAsync(long userId)
         {
+            _logger.LogInformation("Attempting to logout user: {id}", userId);
+
             var user = await _context.Students.FindAsync(userId);
 
             if (user == null) return false;
--- src/backend/Sqeez.Api/Services/AuthService/IAuthService.cs
+++ src/backend/Sqeez.Api/Services/AuthService/IAuthService.cs
@@ -4,7 +4,7 @@ namespace Sqeez.Api.Services.AuthService
 {
     public interface IAuthService
     {
-        Task<bool> RegisterAsync(RegisterDTO dto);
+        Task<string?> RegisterAsync(RegisterDTO dto);
         Task<string?> LoginAsync(LoginDTO loginDto);
         Task<bool> LogoutAsync(long userId);
         Task<UserDTO?> GetCurrentUserAsync(long userId, string? role);

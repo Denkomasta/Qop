a5078047b06e145bb1b146466ffc75f0f834b639
Denny Valach <xvalach3@fi.muni.cz>
2026-04-25 09:52:53 +0200

    feat: add penalty to quizquestion model

---
+++ src/backend/Sqeez.Api/Migrations/20260425074311_AddOptionalPenaltyToQuizQuestion.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sqeez.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOptionalPenaltyToQuizQuestion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Penalty",
+                table: "QuizQuestions",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Penalty",
+                table: "QuizQuestions");
+        }
+    }
+}

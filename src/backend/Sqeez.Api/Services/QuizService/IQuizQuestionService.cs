eaf00065e9d2d0b882a99b7028e0e4a271997e8b
Denny Valach <xvalach3@fi.muni.cz>
2026-04-26 16:32:10 +0200

    fix: quiz get EPs access validation

--- src/backend/Sqeez.Api/Services/QuizService/IQuizQuestionService.cs
+++ src/backend/Sqeez.Api/Services/QuizService/IQuizQuestionService.cs
@@ -4,9 +4,10 @@ namespace Sqeez.Api.Services.Interfaces
 {
     public interface IQuizQuestionService
     {
-        Task<ServiceResult<PagedResponse<QuizQuestionDto>>> GetAllQuizQuestionsAsync(QuizQuestionFilterDto filter);
-        Task<ServiceResult<QuizQuestionDto>> GetQuizQuestionByIdAsync(long id);
-        Task<ServiceResult<DetailedQuizQuestionDto>> GetDetailedQuizQuestionByIdAsync(long id, long quizId);
+        Task<ServiceResult<PagedResponse<QuizQuestionDto>>> GetAllQuizQuestionsAsync(QuizQuestionFilterDto filter, long currentUserId, bool isAdmin);
+        Task<ServiceResult<QuizQuestionDto>> GetQuizQuestionByIdAsync(long id, long currentUserId);
+        Task<ServiceResult<DetailedQuizQuestionDto>> GetDetailedQuizQuestionByIdAsync(long id, long quizId, long currentUserId, string role);
+
         Task<ServiceResult<QuizQuestionDto>> CreateQuizQuestionAsync(CreateQuizQuestionDto dto, long currentUserId);
         Task<ServiceResult<QuizQuestionDto>> PatchQuizQuestionAsync(long id, PatchQuizQuestionDto dto, long currentUserId);
         Task<ServiceResult<bool>> DeleteQuizQuestionAsync(long id, long currentUserId, bool isAdmin);

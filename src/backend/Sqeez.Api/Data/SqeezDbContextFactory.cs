5220c43798f39a7674b775c3e6a44272c5d7452f
Denny Valach <xvalach3@fi.muni.cz>
2026-04-29 17:02:54 +0200

    fix: use factory approach for the migration scripts

---
+++ src/backend/Sqeez.Api/Data/SqeezDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Sqeez.Api.Data;
+
+public class SqeezDbContextFactory : IDesignTimeDbContextFactory<SqeezDbContext>
+{
+    public SqeezDbContext CreateDbContext(string[] args)
+    {
+        DotNetEnv.Env.Load();
+
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+                               ?? "Host=dummy;Database=dummy;";
+
+        var optionsBuilder = new DbContextOptionsBuilder<SqeezDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new SqeezDbContext(optionsBuilder.Options);
+    }
+}
